45af2ffca9a505ab380e00b29ce947ee509938fe
Rahul Dey <rahuldey9623@gmail.com>
2026-05-18 20:32:27 +0530

    Added disable reasoning api call

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-from core.ai_client import AIClient
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(override=True)
 
 
 def main():
-    client = AIClient().get_llama_client()
+    client = OpenAI(
+        base_url=os.getenv("OPENAI_BASE_URL"),
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
 
     response = client.chat.completions.create(
-        model="openai/qwen2.5",
+        model="qwen3:0.6b",
         messages=[
-            {"role": "user", "content": "How many r's are in the word 'strawberry'?"}
+            {"role": "system", "content": "You are a helpful assistant. Give response to the point. NO BS."},
+            {"role": "user", "content": "What is the capital of India?"},
         ],
-        extra_body={"reasoning": {"enabled": False}},
+        max_completion_tokens=100,
+        reasoning_effort="none", # to disable the model thinking
     )
 
     msg = response.choices[0].message.content
     print(f"Response => {msg}")
+    print("=" * 10 + " Usages " + "=" * 10)
+    print(response.usage) # print our the token usages data
 
 
 if __name__ == "__main__":
